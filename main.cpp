1bc4778c4af94a14fc5d0090c50277ae10fa082d
happikin <yashesviraina@outlook.com>
2021-06-08 02:26:14 +0530

    Basic classes defined

--- main.cpp
+++ main.cpp
@@ -7,7 +7,59 @@
 
 #include<iostream>
 using namespace std;
+enum PlayerCount {
+    _ZERO=0,
+    _TWO = 2,
+    _THREE = 3,
+    _FOUR = 4
+};
+enum Sizes {
+    _NULL=0,
+    _8X8=64,
+    _10X10=100,
+    _12X12=144
+};
+class Box {
+    private:
+        PlayerCount playerCount;
+        string *playerNames;
+        bool ladderStart,snakeStart;
+        int ladderSize,snakeSize;
+    public:
+    Box() {
+        playerCount = _ZERO;
+        playerNames = new string[playerCount];
+    }
+    Box() {
+        
+    }
+};
+class Board {
+    private:
+        Sizes boardSize;
+        Box *boardBoxes;
+    public:
+        Board() {
+            boardSize = _NULL;
+            boardBoxes = new Box[boardSize];
+            boardBoxes[0];
+        }
+        Board(Sizes size) {
+            boardSize = size;
+            boardBoxes = new Box[boardSize];
+        }
+        int getSize() {
+            int i = 0;
+            while(i < boardSize) {
+                i++;
+            }
+            return i;
+        }
+
+};
+
 
 int main() {
-    
+    Board mokshaPatam(_8X8);
+    cout<<mokshaPatam.getSize();
 }
