3bbb2032eb99c3f0757d13435bf4df0e63193048
happikin <yashesviraina@outlook.com>
2021-06-07 17:42:45 +0530

    Started the moksha patam project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * Moksha Pata is an ancient but a very famous board game even in todays modern world.
+ * This game has its origin from India and is more popularly called as 
+ * Snakes and Ladders or Chutes and Ladders
+ *
+ */
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+}
